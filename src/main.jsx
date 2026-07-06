5f90e329a24621b505e10fa37624d5a69ccae2c8
Ivo <iyakahyakin@gmail.com>
2026-07-06 07:48:17 +0700

    10

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
 import axios from 'axios';
 
 const rawApiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 const apiUrl = rawApiUrl.replace(/\/$/, '');
-
 
 axios.interceptors.request.use((config) => {
   if (config.url) {
@@ -38,3 +30,8 @@ window.fetch = async function (url, options) {
   return originalFetch(url, options);
 };
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
